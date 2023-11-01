99886459ffc946a776874c648d49388592a07777
AjaySi <ajay.calsoft@gmail.com>
2023-11-01 22:12:33 +0530

    UI design changes and Added HTML format output.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, render_template, redirect, url_for
 from pytube import YouTube
 import tempfile
 import openai
+from html2image import Html2Image
 
 # Set the OpenAI API key
 openai.api_key = "[INSERT YOUR OPENAI API KEY HERE]"
@@ -14,11 +15,16 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/transcribe", methods=["POST"])
+@app.route("/transcribe", methods=["GET", "POST"])
 def transcribe():
+    app.logger.info("Starting ")
     video_url = request.form.get("video_url")
     openai_key = request.form.get("openai_key")
-
+    # Split the video link to get the video ID
+    vid_id = video_url.split("=")[1]
+    hti = Html2Image(output_path="static/css/images")
+    hti.screenshot(url=video_url, save_as=f"yt-img-{vid_id}.png")
+    app.logger.info(f"Saved the YT screenshot:{vid_id}")
     if not openai_key:
         return jsonify({"error": "OpenAI API key is required."}), 400
     else:
@@ -29,6 +35,7 @@ def transcribe():
 
     try:
         # Download YouTube video
+        app.logger.info("Download the YT video.")
         yt = YouTube(video_url)
         stream = yt.streams.filter(only_audio=True).first()
 
@@ -39,14 +46,18 @@ def transcribe():
         stream.download(output_path=os.path.dirname(temp_file_name), filename=os.path.basename(temp_file_name))
 
         # Transcribe the video using OpenAI's Whisper API
+        app.logger.info(f"Saved the YT video: {temp_file_name}, transcribing..")
         with open(temp_file_name, "rb") as audio_file:
             transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
         youtube_blog_content = summarize_youtube_video(transcript)
+        # Convert the output to HTML format.
+        app.logger.info(f"Convert the markdown blog into HTML.")
+        blog_html = convert_markdown_to_html(youtube_blog_content)
         # Remove the temporary file after transcription
         os.remove(temp_file_name)
 
-        return render_template("index.html", transcription=youtube_blog_content)
+        return render_template("index.html", img_src=f"static/css/images/yt-img-{vid_id}.png", transcription=blog_html)
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -83,36 +94,34 @@ def summarize_youtube_video(transcript):
 
   # Generate a summary of the transcript
   summarized_text = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=[
-            {"role": "system",
-            "content": """Act as an expert copywriter specializing in content optimization for SEO. Your task is to take a given YouTube    transcript and transform it into a well-structured and engaging article. Your objectives are as follows:
+            model = "gpt-3.5-turbo-16k",
+            messages = [
+                {
+        "role": "system",
+        "content": """
+        You are an expert copywriter specializing in content optimization for SEO. Your task is to transform a given YouTube transcript into a well-structured and engaging article. Your objectives include:
 
-    Content Transformation: Begin by thoroughly reading the provided YouTube transcript. Understand the main ideas, key points, and the overall message conveyed.
+        1. Master the Transcript: Understand main ideas, key points, and the core message.
+        2. Sentence Structure: Rephrase while preserving logical flow and coherence.
+        3. Identify Main Keyword: Determine the primary video topic.
+        4. Keyword Integration: Naturally integrate keywords in headings, subheadings, and body text, avoiding overuse.
+        5. Write Unique Content: Avoid direct copying; rewrite in your own words.
+        6. Optimize for SEO: Implement meta tags, header tags, and appropriate keyword density.
+        7. Craft Engaging and Informative Article: Provide value and insight to readers.
+        8. Proofread: Check for grammar, spelling, and punctuation errors.
+        9. Use Creative and Human-like Style: Incorporate contractions, idioms, transitional phrases, interjections, and colloquialisms. Avoid repetitive phrases and unnatural sentence structures.
+        10. Structuring: Include a Creative Title, SEO Meta-description, ## Introduction ##, bullet points or numbered lists if appropriate, FAQs, and Conclusion.
+        11. Ensure Uniqueness: Guarantee the article is plagiarism-free.
+        12. Punctuation: Use appropriate question marks at the end of questions.
+        13. Pass AI Detection Tools: Create content that easily passes AI plagiarism detection tools.
 
-    Sentence Structure: While rephrasing the content, pay careful attention to sentence structure. Ensure that the article flows logically and coherently.
-
-    Keyword Identification: Identify the main keyword or phrase from the transcript. Its crucial to determine the primary topic that the YouTube video discusses.
-
-    Keyword Integration: Incorporate the identified keyword naturally throughout the article. Use it in headings, subheadings, and within the body text. However, avoid overuse or keyword stuffing, as this can negatively affect SEO.
-
-    Unique Content: Your goal is to make the article 100% unique. Avoid copying sentences directly from the transcript. Rewrite the content in your own words while retaining the original message and meaning.
-
-    SEO Friendliness: Craft the article with SEO best practices in mind. This includes optimizing meta tags (title and meta description), using header tags appropriately, and maintaining an appropriate keyword density.
-
-    Engaging and Informative: Ensure that the article is engaging and informative for the reader. It should provide value and insight on the topic discussed in the YouTube video.
-        Proofreading: Proofread the article for grammar, spelling, and punctuation errors. Ensure it is free of any mistakes that could detract from its quality.
-
-    By following these guidelines, create a well-optimized, unique, and informative article that would rank well in search engine results and engage readers effectively."""
-            },
-            {
+        Follow these guidelines to create a well-optimized, unique, and informative article that will rank well in search engine results and engage readers effectively.
+        """
+    },  
+          {
                 "role": "user",
-                "content": f"""Craft blog content for the text given below.
-                Text: {transcript}
-
-                Add a title, make sure your content has useful and true information about the main points of the topic.
-                Begin with a introduction explaining the topic. If you can, use bullet points to list important details,
-                and finish your content with a summary.""",
+                "content": f"""Craft blog content for the given Text.
+                Text: {transcript}""",
             },
         ],
         max_tokens=4096,
@@ -123,6 +132,44 @@ def summarize_youtube_video(transcript):
     return summarized_text["choices"][0]["message"]["content"]
   else:
     return None
+
+
+def convert_markdown_to_html(md_content):
+    """ Helper function to convert given text to HTML 
+    """
+    html_response = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo-16k",
+          messages=[ 
+            {"role": "system", "content": """
+Convert Markdown to HTML:
+You are a skilled developer tasked with converting a Markdown-formatted text to HTML. You will be given text in markdown format. Follow these steps to perform the conversion:
+
+1. Parse User's Markdown Input: You will receive a Markdown-formatted text as input from the user. Carefully analyze the provided Markdown text, paying attention to different elements such as headings (#), lists (unordered and ordered), bold and italic text, links, images, and code blocks.
+2. Generate and Validate HTML: Generate corresponding HTML code for each Markdown element following the conversion guidelines below. Ensure the generated HTML is well-structured and syntactically correct.
+3. Preserve Line Breaks: Markdown line breaks (soft breaks) represented by two spaces at the end of a line should be converted to <br> tags in HTML to preserve the line breaks.
+4. REMEMBER to generate complete, valid HTML response only.
+
+Follow below Conversion Guidelines:
+- Headers: Convert Markdown headers (#, ##, ###, etc.) to corresponding HTML header tags (<h1>, <h2>, <h3>, etc.).
+- Lists: Convert unordered lists (*) and ordered lists (1., 2., 3., etc.) to <ul> and <ol> HTML tags, respectively. List items should be enclosed in <li> tags.
+- Emphasis: Convert bold (**) and italic (*) text to <strong> and <em> HTML tags, respectively.
+- Links: Convert Markdown links ([text](url)) to HTML anchor (<a>) tags. Ensure the href attribute contains the correct URL.
+- Images: Convert Markdown image tags (![alt text](image_url)) to HTML image (<img>) tags. Include the alt attribute for accessibility.
+- Code: Convert inline code (`code`) to <code> HTML tags. Convert code blocks (```) to <pre> HTML tags for preserving formatting.
+- Blockquotes: Convert blockquotes (>) to <blockquote> HTML tags.
+"""
+},
+            {"role": "user", "content": f"Convert the following Markdown text to HTML:\n\n{md_content}"}
+        ],
+          max_tokens=8192,
+          temperature=1,
+          n=1,
+    )
+    logger.info("Finished converting markdown to html.")
+    if "choices" in html_response and len(html_response["choices"]) > 0:
+      return html_response["choices"][0]["message"]["content"]
+    else:
+      return None
 
 
 if __name__ == "__main__":
